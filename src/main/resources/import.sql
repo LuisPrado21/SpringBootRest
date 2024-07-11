ece3f490ec412bb08efabeafb1e620abbd89fb20
LuisPrado21 <edupradov5@gmail.com>
2024-07-11 03:19:16 -0500

    Initial commit

---
+++ src/main/resources/import.sql
@@ -0,0 +1,22 @@
+INSERT INTO fabricante(id, nombre) Values(1, 'Asus');
+INSERT INTO fabricante(id, nombre) Values(2, 'Lenovo');
+INSERT INTO fabricante(id, nombre) Values(3, 'Hawlett-Packard');
+INSERT INTO fabricante(id, nombre) Values(4, 'Samsung');
+INSERT INTO fabricante(id, nombre) Values(5, 'Seagate');
+INSERT INTO fabricante(id, nombre) Values(6, 'Crucial');
+INSERT INTO fabricante(id, nombre) Values(7, 'gigabyte');
+INSERT INTO fabricante(id, nombre) Values(8, 'Huaawei');
+INSERT INTO fabricante(id, nombre) Values(9, 'Xiaomi');
+
+
+INSERT INTO producto(id, nombre, precio, id_fabricante) VALUES(1, 'Disco Duro SATA 1TB', 96.99, 5);
+INSERT INTO producto(id, nombre, precio, id_fabricante) VALUES(2, 'Memoria RAM DDR4 8GB', 120, 6);
+INSERT INTO producto(id, nombre, precio, id_fabricante) VALUES(3, 'Disco SSD 1TB', 150.99, 4);
+INSERT INTO producto(id, nombre, precio, id_fabricante) VALUES(4, 'Geforce GTX 1050Ti', 185, 7);
+INSERT INTO producto(id, nombre, precio, id_fabricante) VALUES(5, 'Geforce GTX 1080 Xtreme', 755, 6);
+INSERT INTO producto(id, nombre, precio, id_fabricante) VALUES(6, 'Monitor 24 LED Full HD', 202, 1);
+INSERT INTO producto(id, nombre, precio, id_fabricante) VALUES(7, 'Monitor 27 LED Full HD', 245.99, 1);
+INSERT INTO producto(id, nombre, precio, id_fabricante) VALUES(8, 'Portatil Yoga 520', 559, 2);
+INSERT INTO producto(id, nombre, precio, id_fabricante) VALUES(9, 'Portaatil Yoga 520', 444, 2);
+INSERT INTO producto(id, nombre, precio, id_fabricante) VALUES(10, 'Impresora HP', 59.99, 3);
+INSERT INTO producto(id, nombre, precio, id_fabricante) VALUES(11, 'Impresora HP PRO', 180, 3);
